84b46962c0f0d5fb305b2b24ad976ee97c23d637
Rishabh Kant <krrishabhkant@gmail.com>
2026-01-14 23:50:23 +0530

    canvas init

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+import { createCanvas } from "canvas";
 import express from "express";
 const app = express();
 const port = 3000;
 
 
-app.get('/', (req, res)=>{
-    res.send("working");
+app.get('image', (req, res)=>{
+    const width = parseInt(req.query.width);
+    const height = parseInt(req.query.height) ;
+
+    if(!width || !height){
+        return res.status(400).send("Width and height required");
+    }
+
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#1a1a1a"
+    ctx.fillRect = (0,0 ,width, height);
+
+    
 })
 
 app.listen(port, ()=>{
